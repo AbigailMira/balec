0487236efedf224712aade81d515580b61c5de28
Abigail Mira <miracrick@gmail.com>
2022-11-01 13:03:35 +0100

    database creation

---
+++ src/Entity/Size.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SizeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SizeRepository::class)]
+class Size
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $unit = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $area = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $width = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $length = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $fits = null;
+
+    #[ORM\OneToMany(mappedBy: 'size', targetEntity: Place::class)]
+    private Collection $places;
+
+    #[ORM\OneToMany(mappedBy: 'size', targetEntity: Item::class)]
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->places = new ArrayCollection();
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUnit(): ?string
+    {
+        return $this->unit;
+    }
+
+    public function setUnit(string $unit): self
+    {
+        $this->unit = $unit;
+
+        return $this;
+    }
+
+    public function getArea(): ?float
+    {
+        return $this->area;
+    }
+
+    public function setArea(?float $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(?float $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getLength(): ?float
+    {
+        return $this->length;
+    }
+
+    public function setLength(?float $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getFits(): ?string
+    {
+        return $this->fits;
+    }
+
+    public function setFits(?string $fits): self
+    {
+        $this->fits = $fits;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Place>
+     */
+    public function getPlaces(): Collection
+    {
+        return $this->places;
+    }
+
+    public function addPlace(Place $place): self
+    {
+        if (!$this->places->contains($place)) {
+            $this->places->add($place);
+            $place->setSize($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlace(Place $place): self
+    {
+        if ($this->places->removeElement($place)) {
+            // set the owning side to null (unless already changed)
+            if ($place->getSize() === $this) {
+                $place->setSize(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Item>
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(Item $item): self
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setSize($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(Item $item): self
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getSize() === $this) {
+                $item->setSize(null);
+            }
+        }
+
+        return $this;
+    }
+}
